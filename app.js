b765d9d23982f047859cc162746a4690d954302d
rix1 <rikardeide@gmail.com>
2015-04-20 17:54:42 +0200

    Added server db settings

--- app.js
+++ app.js
@@ -5,13 +5,24 @@ var io = require('socket.io').listen(server);
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db =  monk('localhost:27017/bryllup');
 
+
+var debug = true;
+
+// In production
+if(!debug){
+  var db = monk('localhost:27314/bryllup', {
+    username : 'nodeUser',
+    password : 'grLeTtkq6AZv4bWD3hGkg8@8vuM;/boC#mY2D&.mv'
+  });
+}else{
+  var db =  monk('localhost:27017/bryllup');
+}
 
 app.set('port', 8880);
 
 server.listen(app.get('port'), function(){
-   console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port'));
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -20,23 +31,22 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/down.html');
+  //res.sendFile(__dirname + '/index.html');
 
-  //var collection = db.get("rsvp");
-  //collection.find({}, function (e, docs) {
-  //  res.json(docs);
-  //});
-
+  var collection = db.get("rsvp");
+  collection.find({}, function (e, docs) {
+    res.json(docs);
+  });
 
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  
+
   socket.on('input fields', function(data){
     console.log('data received: '+ data.owl + " " + data.xml);
     saveFile(data.xml);
