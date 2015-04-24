6ddfbb859b795a42d94131885cb29c27f9f51c9f
rix1 <rikardeide@gmail.com>
2015-04-24 19:36:48 +0200

    gunanr

---
+++ app/scripts/controllers/rsvp.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mnBryllupApp.controller:RsvpctrlCtrl
+ * @description
+ * # RsvpctrlCtrl
+ * Controller of the mnBryllupApp
+ */
+angular.module('mnBryllupApp')
+  .controller('RsvpctrlCtrl', function ($scope) {
+
+  });
